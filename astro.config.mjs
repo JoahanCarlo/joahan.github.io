a458ea005e2cfc1419b1698bca96c1944346b38e
Joahan Carlo Nuñez Soto <140027727+JoahanCarlo@users.noreply.github.com>
2025-06-26 01:04:54 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import siteConfig from './src/data/site-config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: siteConfig.website,
+    site: 'https://github.com/JoahanCarlo',
     vite: {
         plugins: [tailwindcss()]
     },
